b18a7feb038d3e189dffdc408fdc268f96a9077f
shige ito <sito@winlight.co.jp>
2018-01-14 11:58:22 +0900

    プロジェクト ファイルを追加します。

---
+++ CsConsole2180114/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsConsole2180114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
